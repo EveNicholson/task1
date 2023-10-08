6ab7e06af5937b23b73f64a15a1e2dbbfddf1b0e
Ewelina Nicholson <e.guzikk@gmail.com>
2023-10-08 15:40:04 +0100

    first commit

---
+++ static_and_dynamic_task1.py
@@ -0,0 +1,32 @@
+
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    if card.value = 1:
+      return True
+    else
+      return False
+   
+# Should be two == to check if two objects have the same value
+# Missing colon after else. It tells python the next line of intended block should only be run if condition is true
+
+  dif highest_card(self, card1 card2):
+    if card1.value > card2.value:
+        return card
+    else:
+        return card2
+    
+# Missing comma between function arguments so they are not separated
+# In return statement card is not specified in our function arguments
+# At the beginning of this function there is dif instead of def which is a keyword to define a function
+
+ def cards_total(self, cards):
+  total
+  for card in cards:
+    total += card.value
+    return "You have a total of" + total
+  
+
+# There is no appropriate indent added to our function
+# Total is unbound which means it is not assigned( should be total=0)
